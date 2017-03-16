cbc8cde90bd60a5c78845c4d94c51fb1764ee8f4
Евсюкова Арина <risha1rush1@gmail.com>
2017-03-16 22:36:58 +0300

    Добавьте файлы проекта.

---
+++ Ski Resorts/Ski_Resort.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ski_Resorts
+{
+    class Ski_Resort
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _country;
+
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+
+        private string _nearby_cities;
+
+        public string Nearby_Cities
+        {
+            get { return _nearby_cities; }
+            set { _nearby_cities = value; }
+        }
+
+        private int _highest_peak;
+
+        public int _Highest_Peak
+        {
+            get { return _highest_peak; }
+            set { _highest_peak = value; }
+        }
+
+        private int _heigh_difference;
+
+        public int Heigh_Difference
+        {
+            get { return _heigh_difference; }
+            set { _heigh_difference = value; }
+        }
+
+        private int _km;
+
+        public int Km
+        {
+            get { return _km; }
+            set { _km = value; }
+        }
+
+        private int _km_green;
+
+        public int Km_Green
+        {
+            get { return _km_green; }
+            set { _km_green = value; }
+        }
+
+        private int _km_blue;
+
+        public int Km_Blue
+        {
+            get { return _km_blue; }
+            set { _km_blue = value; }
+        }
+
+        private int _km_red;
+
+        public int Km_red
+        {
+            get { return _km_red; }
+            set { _km_red = value; }
+        }
+
+        private int _km_black;
+
+        public int Km_Black
+        {
+            get { return _km_black; }
+            set { _km_black = value; }
+        }
+
+        private int _the_longest_slope;
+
+        public int The_Longest_Slope
+        {
+            get { return _the_longest_slope; }
+            set { _the_longest_slope = value; }
+        }
+
+        private int _ski_lifts;
+
+        public int Ski_Lifts
+        {
+            get { return _ski_lifts; }
+            set { _ski_lifts = value; }
+        }
+
+        private int _snowparks;
+
+        public int Snowparks
+        {
+            get { return _snowparks; }
+            set { _snowparks = value; }
+        }
+
+        private int _skipass;
+
+        public int Skipass
+        {
+            get { return _skipass; }
+            set { _skipass = value; }
+        }
+    }
+}
